e5bfde16fd65c631542997706b93cff48d114c03
Rsoniie <roshansoniee@gmail.com>
2025-04-11 15:39:39 +0530

    Condition Checking function done

--- server/services/addAll.js
+++ server/services/addAll.js
@@ -1,6 +1,6 @@
 import generateRandomHealthData from "../utils/genData.js";
 import User from "../models/userModel.js";
-import evaluateHealthCondition from "../utils/criticalCheck.js";
+import evaluateHealth from "../utils/criticalCheck.js";
 import Parent from "../models/parentModel.js";
 import sendMail from "../utils/sendMail.js";
 
@@ -10,13 +10,15 @@ const addAll = async (req, res) => {
     const updatedUsers = await Promise.all(
       users.map(async (user) => {
         const healthData = generateRandomHealthData();
-        const critical = evaluateHealthCondition(healthData);
+        console.log("Just before calling health check")
+        const critical = evaluateHealth(healthData);
+        console.log("This is output from health_check function", critical)
 
         if (critical == "Critical") {
           const parent_id = user.parent;
           const parent = await Parent.findById(parent_id);
           const email = parent.email;
-          sendMail(email);
+          // sendMail(email);
         }
 
         healthData["condition"] = critical;
--- server/utils/genData.js
+++ server/utils/genData.js
@@ -1,21 +1,26 @@
 function generateRandomHealthData() {
-    const heartRate = Math.floor(Math.random() * (100 - 60 + 1)) + 60;
-    const bloodPressure = `${Math.floor(Math.random() * (140 - 90 + 1)) + 90}/${Math.floor(Math.random() * (90 - 60 + 1)) + 60}`;
-    const sleepQuality = Math.random() > 0.5 ? 'Good' : 'Poor'; 
-    const oxygenLevel = Math.floor(Math.random() * (100 - 90 + 1)) + 90;
-    const stressLevel = Math.floor(Math.random() * (10 - 1 + 1)) + 1;
-    const temperature = (Math.random() * (99.5 - 95.0) + 95.0).toFixed(1); 
+  const heartRate = Math.floor(Math.random() * (100 - 60 + 1)) + 60;
+  const bloodPressure = `${Math.floor(Math.random() * (140 - 90 + 1)) + 90}/${
+    Math.floor(Math.random() * (90 - 60 + 1)) + 60
+  }`;
+  // const sleepQuality = Math.random() > 0.5 ? 'Good' : 'Poor';
+  const options = ["Good", "Average", "Poor"];
+  const sleepQuality = options[Math.floor(Math.random() * options.length)];
 
-    const healthData = {
-        heart_rate: heartRate,
-        bloodpressure: bloodPressure,
-        oxygen_level: oxygenLevel,
-        sleep_quality: sleepQuality,
-        temperature: temperature,
-        stress_level: stressLevel,
-    };
+  const oxygenLevel = Math.floor(Math.random() * (100 - 90 + 1)) + 90;
+  const stressLevel = Math.floor(Math.random() * (10 - 1 + 1)) + 1;
+  const temperature = (Math.random() * (99.5 - 95.0) + 95.0).toFixed(1);
 
-    return healthData;
+  const healthData = {
+    heart_rate: heartRate,
+    bloodpressure: bloodPressure,
+    oxygen_level: oxygenLevel,
+    sleep_quality: sleepQuality,
+    temperature: temperature,
+    stress_level: stressLevel,
+  };
+
+  return healthData;
 }
 
 export default generateRandomHealthData;
