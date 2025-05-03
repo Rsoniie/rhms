0e88847766a11375bb00e9f391bdf89bdc0fee45
Rsoniie <roshansoniee@gmail.com>
2025-05-03 14:38:38 +0530

    Schema done and all data generated

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,3 +1,62 @@
+// import mongoose from "mongoose";
+
+// const userSchema = new mongoose.Schema({
+//     email: {
+//         type: String,
+//         required: true,
+//         unique: true,
+//         trim: true,
+//     },
+//     password: {
+//         type: String,
+//         required: true,
+//     },
+//     username: {
+//         type: String,
+//         required: true,
+//         trim: true,
+//         unique: true,
+//     },
+//     health_data: [
+//         {
+//             heart_rate: {
+//                 type: Number,
+//             },
+//             bloodpressure: {
+//                 type: String,
+//             },
+//             oxygen_level: {
+//                 type: Number,
+//             },
+//             sleep_quality: {
+//                 type: String,
+//             },
+//             temperature: {
+//                 type: Number,
+//             },
+//             stress_level: {
+//                 type: String,
+//             },
+//             condition : {
+//                 type: String,
+//                 default: "Normal",
+//             },
+//             recorded_at: {
+//                 type: Date,
+//                 default: Date.now,
+//             },
+//         }
+//     ],
+//     parent : {
+//         type: mongoose.Schema.Types.ObjectId,
+//         ref: 'Parent',
+//     },
+// }, { timestamps: true });
+
+// const User =  mongoose.model('User', userSchema);
+// export default User;
+
+
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
@@ -22,7 +81,7 @@ const userSchema = new mongoose.Schema({
             heart_rate: {
                 type: Number,
             },
-            bloodpressure: {
+            blood_pressure: {  // fixed naming to match function
                 type: String,
             },
             oxygen_level: {
@@ -34,10 +93,19 @@ const userSchema = new mongoose.Schema({
             temperature: {
                 type: Number,
             },
-            stress_level: {
-                type: String,
+            stress_level: {  // changed from String → Number
+                type: Number,
             },
-            condition : {
+            total_daily_steps: {  // added field
+                type: Number,
+            },
+            active_minutes: {  // added field
+                type: Number,
+            },
+            calories_burnt: {  // added field
+                type: Number,
+            },
+            condition: {
                 type: String,
                 default: "Normal",
             },
@@ -47,11 +115,11 @@ const userSchema = new mongoose.Schema({
             },
         }
     ],
-    parent : {
+    parent: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Parent',
     },
 }, { timestamps: true });
 
-const User =  mongoose.model('User', userSchema);
+const User = mongoose.model('User', userSchema);
 export default User;
--- server/utils/genData.js
+++ server/utils/genData.js
@@ -3,21 +3,25 @@ function generateRandomHealthData() {
   const bloodPressure = `${Math.floor(Math.random() * (140 - 90 + 1)) + 90}/${
     Math.floor(Math.random() * (90 - 60 + 1)) + 60
   }`;
-  // const sleepQuality = Math.random() > 0.5 ? 'Good' : 'Poor';
   const options = ["Good", "Average", "Poor"];
   const sleepQuality = options[Math.floor(Math.random() * options.length)];
-
   const oxygenLevel = Math.floor(Math.random() * (100 - 90 + 1)) + 90;
   const stressLevel = Math.floor(Math.random() * (10 - 1 + 1)) + 1;
   const temperature = (Math.random() * (99.5 - 95.0) + 95.0).toFixed(1);
+  const totalDailySteps = Math.floor(Math.random() * (20000 - 1000 + 1)) + 1000;
+  const activeMinutes = Math.floor(Math.random() * (300 - 30 + 1)) + 30;
+  const caloriesBurnt = Math.floor(Math.random() * (4000 - 1500 + 1)) + 1500;
 
   const healthData = {
     heart_rate: heartRate,
-    bloodpressure: bloodPressure,
+    blood_pressure: bloodPressure,
     oxygen_level: oxygenLevel,
     sleep_quality: sleepQuality,
     temperature: temperature,
     stress_level: stressLevel,
+    total_daily_steps: totalDailySteps,
+    active_minutes: activeMinutes,
+    calories_burnt: caloriesBurnt,
   };
 
   return healthData;
